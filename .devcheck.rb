0052ca04c7d5bf3386fd1078ee1a9632a51c4bc2
Louie Bloomberg <louie@lnbloomberg.net>
2026-09-22 22:12:41 -0500

    created .devcheck.rb with specified commands and files to be checked

---
+++ .devcheck.rb
@@ -0,0 +1,18 @@
+# .devcheck.rb
+# Copyright (c) 2026 Louie Bloomberg.
+# SPDX-License-Identifier: MIT
+
+command "python3"
+command "git"
+command "pip3"
+
+file "LICENSE"
+file "README.md"
+file "THIRD-PARTY-NOTICES.txt"
+file "requirements.txt"
+file "app.py"
+file "analyzer.py"
+file "static/app.js"
+file "templates/index.html"
+file "templates/license.html"
+file "static/style.css"
